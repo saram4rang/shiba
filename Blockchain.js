013b8794cd78d7f82a36b6b68e1bd0c1ac6984fd
Kung Fu Ant <kungfuant@gmail.com>
2014-11-06 19:59:51 +0100

    Pings, timeouts and reconnecting for the Blockchain API.

--- Blockchain.js
+++ Blockchain.js
@@ -8,38 +8,60 @@ module.exports = Blockchain;
 
 function Blockchain() {
   _.extend(this, Events);
-  this.socket = new WebSocket('ws://ws.blockchain.info/inv');
-  this.socket.on('open', this.onOpen.bind(this));
-  this.socket.on('message', this.onMessage.bind(this));
-  this.socket.on('error', this.onError.bind(this));
-  this.socket.on('close', this.onClose.bind(this));
+  this.pingTimeoutTimer  = null;
+  this.pingTimeout       = 5000;
+  this.pingIntervalTimer = null;
+  this.pingInterval      = 30000;
+  this.reconnectInterval = 30000;
+  this.doConnect();
 }
 
 util.inherits(Blockchain, Events.EventEmitter);
 
+Blockchain.prototype.doConnect = function() {
+  debug('Connecting to Blockchain API.');
+  var self = this;
+  var socket = new WebSocket('ws://ws.blockchain.info/inv');
+  socket.on('open', function() {
+    self.socket = socket;
+    self.socket.on('message', self.onMessage.bind(self));
+    self.socket.on('error', self.onError.bind(self));
+    self.socket.on('close', self.onClose.bind(self));
+    self.socket.on('pong', self.onPong.bind(self));
+    self.onOpen();
+  });
+};
+
 Blockchain.prototype.onOpen = function() {
   debug('Connection established.');
-  
+
   // Subscribe to new blocks.
   this.socket.send('{"op":"blocks_sub"}', this.onError.bind(this));
 
   // Get the latest block.
   this.socket.send('{"op":"ping_block"}', this.onError.bind(this));
+
+  this.resetPingTimer();
   this.emit('connect');
 };
 
+
 Blockchain.prototype.onMessage = function(message, flags) {
   var data = JSON.parse(message);
-  debug('Op received', data.op);
+  debug("Op received: '%s'.", data.op);
 
   switch (data.op) {
   case 'status':
     debug('Status %s', message);
     break;
   case 'block':
-    this.doBlock(data.x);
+    var block = data.x;
+    debug('New block #%d, time %d.', block.height, block.time);
+    this.emit('block', block);
     break;
   }
+
+  this.resetPingTimer();
 };
 
 Blockchain.prototype.onError = function(error) {
@@ -47,11 +69,40 @@ Blockchain.prototype.onError = function(error) {
 };
 
 Blockchain.prototype.onClose = function(code, message) {
-  debug('Connection closed.');
+  debug('Connection closed. Reconnecting in %d ms.', this.reconnectInterval);
+  setTimeout(this.doConnect.bind(this), this.reconnectInterval);
+
+  clearTimeout(this.pingIntervalTimer);
+  clearTimeout(this.pingTimeoutTimer);
+  this.socket.removeAllListeners();
+  this.socket = null;
   this.emit('disconnect');
 };
 
-Blockchain.prototype.doBlock = function(block) {
-  debug('New block #%d Time', block.height, block.time);
-  this.emit('block', block);
+Blockchain.prototype.resetPingTimer = function() {
+  debug('Resetting ping interval timer: %d ms.', this.pingInterval);
+  clearTimeout(this.pingIntervalTimer);
+  clearTimeout(this.pingTimeoutTimer);
+  this.pingIntervalTimer =
+    setTimeout(this.onPingInterval.bind(this),
+               this.pingInterval);
+};
+
+
+Blockchain.prototype.onPingInterval = function() {
+  debug('Ping interval. Sending ping. Timeout: %d ms.', this.pingTimeout);
+  this.socket.ping();
+  this.pingTimeoutTimer =
+    setTimeout(this.onPingTimeout.bind(this),
+               this.pingTimeout);
+};
+
+Blockchain.prototype.onPingTimeout = function() {
+  debug('Ping timed out. Closing connection.');
+  this.socket.close();
+};
+
+Blockchain.prototype.onPong = function() {
+  debug('Pong received.');
+  this.resetPingTimer();
 };
