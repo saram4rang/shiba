504efb2d035d788d8b6ae154160cc4b742806254
Kung Fu Ant <kungfuant@gmail.com>
2015-06-10 16:40:24 +0000

    Limit !median to 100k

--- Cmd/Median.js
+++ Cmd/Median.js
@@ -10,12 +10,19 @@ Median.prototype.handle = function*(client, msg, input) {
 
   debug('Handling median for "%s" games', input);
 
-  if (!/[0-9]+/.test(input)) {
+  let match = input.match(/^([0-9]+)(k|m)$/i);
+  if (!match) {
     client.doSay('wow. very usage failure. such retry');
     return;
   }
 
-  let numGames = parseInt(input);
+  let numGames = parseInt(match[1]);
+  switch(match[2].toLowerCase()) {
+  case 'm': numGames *= 1000;
+  case 'k': numGames *= 1000;
+  }
+  numGames = Math.min(1e5, numGames);
+
   let result   = yield* Pg.getGameCrashMedian(numGames);
   let response = 'Median over last ' +
         result.count + ' games: ' + result.median/100 + 'x';
