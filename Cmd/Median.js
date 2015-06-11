85d5a2da13d088ea658f2cb9649cf314089167ea
Kung Fu Ant <kungfuant@gmail.com>
2015-06-11 12:55:34 +0000

    Fix !median command

--- Cmd/Median.js
+++ Cmd/Median.js
@@ -10,14 +10,14 @@ Median.prototype.handle = function*(client, msg, input) {
 
   debug('Handling median for "%s" games', input);
 
-  let match = input.match(/^([0-9]+)(k|m)$/i);
+  let match = input.match(/^([0-9]+)(k|m)?$/i);
   if (!match) {
     client.doSay('wow. very usage failure. such retry');
     return;
   }
 
   let numGames = parseInt(match[1]);
-  switch(match[2].toLowerCase()) {
+  switch(match[2] && match[2].toLowerCase()) {
   case 'm': numGames *= 1000;
   case 'k': numGames *= 1000;
   }
