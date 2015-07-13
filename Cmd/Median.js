4ce04cf7b336bda8f662b28b11f3030474bc8a92
Kung Fu Ant <kungfuant@gmail.com>
2015-07-13 22:01:44 +0000

    Better !median command

--- Cmd/Median.js
+++ Cmd/Median.js
@@ -6,26 +6,42 @@ const Pg    = require('../Pg');
 function Median() {
 }
 
+function parse(input) {
+  var nums = [];
+
+  for (;;) {
+    let match = input.match(/^([0-9]+)(k|m)?(,\s*|\s+)?(.*)?/i);
+    if (!match)
+      return nums;
+
+    let numGames = parseInt(match[1]);
+    switch(match[2] && match[2].toLowerCase()) {
+    case 'm': numGames *= 1000;
+    case 'k': numGames *= 1000;
+    }
+    numGames = Math.min(1e5, numGames);
+    nums.push(numGames);
+
+    input = match[4] || '';
+  }
+}
+
 Median.prototype.handle = function*(client, msg, input) {
 
   debug('Handling median for "%s" games', input);
 
-  let match = input.match(/^([0-9]+)(k|m)?$/i);
-  if (!match) {
+  let nums = parse(input);
+  if (nums.length <= 0) {
     client.doSay('wow. very usage failure. such retry');
     return;
   }
 
-  let numGames = parseInt(match[1]);
-  switch(match[2] && match[2].toLowerCase()) {
-  case 'm': numGames *= 1000;
-  case 'k': numGames *= 1000;
-  }
-  numGames = Math.min(1e5, numGames);
+  let result   = yield (nums.map(num => Pg.getGameCrashMedian(num)));
+  nums = nums.map(num => '' + num).join(', ');
+  result = result.map(obj => '' + (obj.median/100) + 'x').join(', ');
 
-  let result   = yield* Pg.getGameCrashMedian(numGames);
   let response = 'Median over last ' +
-        result.count + ' games: ' + result.median/100 + 'x';
+        nums + ' games: ' + result;
 
   client.doSay(response);
 };
