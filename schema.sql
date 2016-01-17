d32927696fb7aaac5bd106abad92648b291bcac3
Kung Fu Ant <kungfuant@gmail.com>
2016-01-17 02:07:26 +0100

    Update schema

--- schema.sql
+++ schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE users (
-  id bigint NOT NULL,
+  id bigserial NOT NULL PRIMARY KEY,
   username text NOT NULL,
   gross_profit bigint DEFAULT 0 NOT NULL,
   net_profit bigint DEFAULT 0 NOT NULL,
@@ -7,131 +7,61 @@ CREATE TABLE users (
   -- The timestamp indicates when we have seen the user for the first time.
   created timestamp with time zone DEFAULT now() NOT NULL
 );
-CREATE SEQUENCE users_id_seq
-  START WITH 1
-  INCREMENT BY 1
-  NO MINVALUE
-  NO MAXVALUE
-  CACHE 1;
-ALTER SEQUENCE users_id_seq OWNED BY users.id;
-
 
 CREATE TABLE licks (
-  id bigint NOT NULL,
-  user_id bigint NOT NULL,
+  id bigserial NOT NULL PRIMARY KEY,
+  user_id bigint NOT NULL REFERENCES users(id),
   message text NOT NULL,
-  creator_id bigint NOT NULL,
+  creator_id bigint NOT NULL REFERENCES users(id),
   created timestamp with time zone DEFAULT now() NOT NULL
 );
-CREATE SEQUENCE licks_id_seq
-  START WITH 1
-  INCREMENT BY 1
-  NO MINVALUE
-  NO MAXVALUE
-  CACHE 1;
-ALTER SEQUENCE licks_id_seq OWNED BY licks.id;
-
 
 CREATE TYPE UserClassEnum AS ENUM ('user', 'moderator', 'admin');
 
 CREATE TABLE chats (
-  id bigint NOT NULL,
-  user_id bigint NOT NULL,
+  id bigserial NOT NULL PRIMARY KEY,
+  user_id bigint NOT NULL REFERENCES users(id),
   channel text NOT NULL,
   message text NOT NULL,
   is_bot boolean NOT NULL,
   created timestamp with time zone DEFAULT now() NOT NULL
 );
-CREATE SEQUENCE chats_id_seq
-  START WITH 1
-  INCREMENT BY 1
-  NO MINVALUE
-  NO MAXVALUE
-  CACHE 1;
-ALTER SEQUENCE chats_id_seq OWNED BY chats.id;
-
+CREATE UNIQUE INDEX unique_chats_user_created ON chats USING btree (user_id, created);
 
 CREATE TABLE mutes (
-  id bigint NOT NULL,
-  user_id bigint NOT NULL,
-  moderator_id bigint NOT NULL,
+  id bigserial NOT NULL PRIMARY KEY,
+  user_id bigint NOT NULL REFERENCES users(id),
+  moderator_id bigint NOT NULL REFERENCES users(id),
   timespec text NOT NULL,
   shadow boolean DEFAULT false NOT NULL,
   created timestamp with time zone DEFAULT now() NOT NULL
 );
-CREATE SEQUENCE mutes_id_seq
-  START WITH 1
-  INCREMENT BY 1
-  NO MINVALUE
-  NO MAXVALUE
-  CACHE 1;
-ALTER SEQUENCE mutes_id_seq OWNED BY mutes.id;
-
 
 CREATE TABLE unmutes (
-  id bigint NOT NULL,
-  user_id bigint NOT NULL,
-  moderator_id bigint NOT NULL,
+  id bigserial NOT NULL PRIMARY KEY,
+  user_id bigint NOT NULL REFERENCES users(id),
+  moderator_id bigint NOT NULL REFERENCES users(id),
   shadow boolean DEFAULT false NOT NULL,
   created timestamp with time zone DEFAULT now() NOT NULL
 );
-CREATE SEQUENCE unmutes_id_seq
-  START WITH 1
-  INCREMENT BY 1
-  NO MINVALUE
-  NO MAXVALUE
-  CACHE 1;
-ALTER SEQUENCE unmutes_id_seq OWNED BY unmutes.id;
-
 
 CREATE TABLE games (
-  id bigint NOT NULL,
+  id bigint NOT NULL PRIMARY KEY,
   game_crash bigint NOT NULL,
   seed text,
   created timestamp with time zone DEFAULT now() NOT NULL,
   started timestamp with time zone NULL
 );
-CREATE SEQUENCE games_id_seq
-  START WITH 1
-  INCREMENT BY 1
-  NO MINVALUE
-  NO MAXVALUE
-  CACHE 1;
-ALTER SEQUENCE games_id_seq OWNED BY games.id;
 
 CREATE TABLE plays (
-  id bigint NOT NULL,
-  user_id bigint NOT NULL,
+  id bigserial NOT NULL PRIMARY KEY,
+  user_id bigint NOT NULL REFERENCES users(id),
   cash_out bigint,
-  game_id bigint NOT NULL,
+  game_id bigint NOT NULL REFERENCES games(id),
   bet bigint NOT NULL,
   bonus bigint,
   joined timestamp with time zone NULL
 );
-CREATE SEQUENCE plays_id_seq
-  START WITH 1
-  INCREMENT BY 1
-  NO MINVALUE
-  NO MAXVALUE
-  CACHE 1;
-ALTER SEQUENCE plays_id_seq OWNED BY plays.id;
-
-
-ALTER TABLE ONLY users ALTER COLUMN id SET DEFAULT nextval('users_id_seq'::regclass);
-ALTER TABLE ONLY licks ALTER COLUMN id SET DEFAULT nextval('licks_id_seq'::regclass);
-ALTER TABLE ONLY chats ALTER COLUMN id SET DEFAULT nextval('chats_id_seq'::regclass);
-ALTER TABLE ONLY mutes ALTER COLUMN id SET DEFAULT nextval('mutes_id_seq'::regclass);
-ALTER TABLE ONLY games ALTER COLUMN id SET DEFAULT nextval('games_id_seq'::regclass);
-ALTER TABLE ONLY plays ALTER COLUMN id SET DEFAULT nextval('plays_id_seq'::regclass);
-ALTER TABLE ONLY unmutes ALTER COLUMN id SET DEFAULT nextval('unmutes_id_seq'::regclass);
-
-ALTER TABLE ONLY users ADD CONSTRAINT users_pkey PRIMARY KEY (id);
-ALTER TABLE ONLY licks ADD CONSTRAINT licks_pkey PRIMARY KEY (id);
-ALTER TABLE ONLY chats ADD CONSTRAINT chats_pkey PRIMARY KEY (id);
-ALTER TABLE ONLY mutes ADD CONSTRAINT mutes_pkey PRIMARY KEY (id);
-ALTER TABLE ONLY games ADD CONSTRAINT games_pkey PRIMARY KEY (id);
-ALTER TABLE ONLY plays ADD CONSTRAINT plays_pkey PRIMARY KEY (id);
-ALTER TABLE ONLY unmutes ADD CONSTRAINT unmutes_pkey PRIMARY KEY (id);
 
 CREATE INDEX licks_user_id_idx ON licks USING btree (user_id);
 CREATE INDEX licks_creator_id_idx ON licks USING btree (creator_id);
@@ -146,70 +76,6 @@ CREATE INDEX unmutes_user_id_idx ON unmutes USING btree (user_id);
 CREATE INDEX unmutes_moderator_id_idx ON unmutes USING btree (moderator_id);
 CREATE UNIQUE INDEX unique_plays_game_user ON plays USING btree (user_id, game_id);
 
-ALTER TABLE ONLY licks
-  ADD CONSTRAINT licks_user_id_fkey
-  FOREIGN KEY (user_id)
-  REFERENCES users(id)
-  ON UPDATE CASCADE
-  ON DELETE CASCADE;
-
-ALTER TABLE ONLY licks
-  ADD CONSTRAINT licks_creator_id_fkey
-  FOREIGN KEY (creator_id)
-  REFERENCES users(id)
-  ON UPDATE CASCADE
-  ON DELETE CASCADE;
-
-ALTER TABLE ONLY chats
-  ADD CONSTRAINT chats_user_id_fkey
-  FOREIGN KEY (user_id)
-  REFERENCES users(id)
-  ON UPDATE CASCADE
-  ON DELETE CASCADE;
-
-ALTER TABLE ONLY mutes
-  ADD CONSTRAINT mutes_user_id_fkey
-  FOREIGN KEY (user_id)
-  REFERENCES users(id)
-  ON UPDATE CASCADE
-  ON DELETE CASCADE;
-
-ALTER TABLE ONLY mutes
-  ADD CONSTRAINT mutes_moderator_id_fkey
-  FOREIGN KEY (moderator_id)
-  REFERENCES users(id)
-  ON UPDATE CASCADE
-  ON DELETE CASCADE;
-
-ALTER TABLE ONLY plays
-  ADD CONSTRAINT plays_game_id_fkey
-  FOREIGN KEY (game_id)
-  REFERENCES games(id)
-  ON UPDATE CASCADE
-  ON DELETE CASCADE;
-
-ALTER TABLE ONLY plays
-  ADD CONSTRAINT plays_user_id_fkey
-  FOREIGN KEY (user_id)
-  REFERENCES users(id)
-  ON UPDATE CASCADE
-  ON DELETE CASCADE;
-
-ALTER TABLE ONLY unmutes
-  ADD CONSTRAINT unmutes_user_id_fkey
-  FOREIGN KEY (user_id)
-  REFERENCES users(id)
-  ON UPDATE CASCADE
-  ON DELETE CASCADE;
-
-ALTER TABLE ONLY unmutes
-  ADD CONSTRAINT unmutes_moderator_id_fkey
-  FOREIGN KEY (moderator_id)
-  REFERENCES users(id)
-  ON UPDATE CASCADE
-  ON DELETE CASCADE;
-
-
 -- Game crash table
 
 -- This is derived data to have fast lookups of game crashes without
@@ -221,20 +87,9 @@ ALTER TABLE ONLY unmutes
 --        GROUP BY game_crash);
 
 CREATE TABLE game_crashes (
-  game_crash bigint NOT NULL,
-  id bigint NOT NULL
+  game_crash bigint NOT NULL PRIMARY KEY,
+  id bigint NOT NULL REFERENCES games(id)
 );
-
-ALTER TABLE ONLY game_crashes
-  ADD CONSTRAINT game_crashes_pkey
-  PRIMARY KEY (game_crash);
-
-ALTER TABLE ONLY game_crashes
-  ADD CONSTRAINT game_crashes_id_fkey
-  FOREIGN KEY (id)
-  REFERENCES games(id)
-  ON UPDATE CASCADE
-  ON DELETE CASCADE;
 
 CREATE UNIQUE INDEX game_crashes_crash_idx
   ON game_crashes
@@ -275,35 +130,12 @@ CREATE TRIGGER game_crash_trigger
   FOR EACH ROW EXECUTE PROCEDURE game_crash_trigger();
 
 CREATE TABLE automutes (
-  id bigint NOT NULL,
-  creator_id bigint NOT NULL,
+  id bigserial NOT NULL PRIMARY KEY,
+  creator_id bigint NOT NULL REFERENCES users(id),
   created timestamp with time zone DEFAULT now() NOT NULL,
   regexp text NOT NULL,
   enabled boolean DEFAULT true NOT NULL
 );
-
-CREATE SEQUENCE automutes_id_seq
-  START WITH 1
-  INCREMENT BY 1
-  NO MINVALUE
-  NO MAXVALUE
-  CACHE 1;
-
-ALTER SEQUENCE automutes_id_seq
-  OWNED BY automutes.id;
-
-ALTER TABLE ONLY automutes
-  ALTER COLUMN id
-  SET DEFAULT nextval('automutes_id_seq'::regclass);
-ALTER TABLE ONLY automutes
-  ADD CONSTRAINT automutes_pkey
-  PRIMARY KEY (id);
-ALTER TABLE ONLY automutes
-  ADD CONSTRAINT automutes_creator_id_fkey
-  FOREIGN KEY (creator_id)
-  REFERENCES users(id)
-  ON UPDATE CASCADE
-  ON DELETE CASCADE;
 
 CREATE TABLE blocks (
   height integer NOT NULL,
