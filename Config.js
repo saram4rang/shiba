e318e62afb1ca6d97fa4ab3ec07a1786fff362b8
neko z <zcrash@users.noreply.github.com>
2017-06-21 07:32:22 -0600

    Update Config.js
    zcrash specific updates

--- Config.js
+++ Config.js
@@ -8,9 +8,9 @@ module.exports =
   VERSION:        packageJson.version,
   CLIENT_SEED:
     process.env.BUSTABIT_CLIENTSEED ||
-    '000000000000000007a9a31ff7f07463d91af6b5454241d5faf282e5e0fe1b3a',
-  GAMESERVER:     process.env.BUSTABIT_GAMESERVER || 'https://gs.bustabit.com',
-  WEBSERVER:      process.env.BUSTABIT_WEBSERVER || 'https://www.bustabit.com',
+    '0000000003a96dc6e52672a487ec577fc32e71a8f99bdbbf7329bf27f9772544',
+  GAMESERVER:     process.env.BUSTABIT_GAMESERVER || 'https://gs.zcrash.io',
+  WEBSERVER:      process.env.BUSTABIT_WEBSERVER || 'https://www.zcrash.io',
   OXR_APP_ID:     process.env.OXR_APP_ID,
   SESSION:        process.env.SHIBA_SESSION,
   DATABASE:       process.env.SHIBA_DATABASE || 'postgres://localhost/shibadb',
@@ -18,35 +18,9 @@ module.exports =
   GAME_HISTORY:   process.env.SHIBA_GAME_HISTORY || 200,
   /* keep in lowercase */
   USER_WHITELIST: [
-    '01010100b',
-    'alexk08',
-    'almighty',
-    'beebo',
-    'bitcoininformation',
-    'cooldad',
-    'cowbay',
-    'csm',
-    'delorian',
-    'dexon',
-    'dexonbot',
-    'dmt',
-    'dooglus',
-    'dxc',
-    'gecox22',
     'kungfuant',
-    'lakai',
-    'martinbot',
-    'marting',
-    'neta',
-    'netaban',
-    'qcrc5u4',
-    'rapetor',
     'ryan',
     'shiba',
-    'steve',
-    'techdeck',
-    'turtledaddykim',
-    'wake_up_son',
-    'xrnath'
+    'nekoz'
   ]
 };
